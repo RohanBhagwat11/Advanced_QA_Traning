86bac9f7fb829a0e99a44b742684364c58cb5d4e
RohanBhagwat11 <rohan.bhagwat@zeuslearning.com>
2026-06-25 18:56:54 +0530

    Create mockdata.ts

---
+++ Constants/mockdata.ts
@@ -0,0 +1,15 @@
+export const mockBookData = {
+    books: [
+      {
+        isbn: "9781449331818",
+        title: "Rohan Bhagwat",
+        subTitle: "QA Trainee",
+        author: "Rahul Gupta",
+        publish_date: "2020-06-12T08:48:39.000Z",
+        publisher: "Zeus Learning",
+        pages: 234,
+        description: "This is Dummy Data",
+        website: "https://playwright.dev/python/",
+      },
+    ],
+  };
