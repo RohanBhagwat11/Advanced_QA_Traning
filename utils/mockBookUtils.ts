1c88fa3514acb63f0296130bad2cf0400f8ba693
RohanBhagwat11 <rohan.bhagwat@zeuslearning.com>
2026-06-25 18:59:03 +0530

    Update mockBookUtils.ts

--- utils/mockBookUtils.ts
+++ utils/mockBookUtils.ts
@@ -1,32 +1,16 @@
 // utils/BookMockUtils.ts
 import { Page } from "@playwright/test";
+import { mockBookData } from "../Constants/mockdata";
 
 export class BookMockUtils {
   private page: Page;
-
-  // Standard dummy book data reused across tests
-  public mockBookData = {
-    books: [
-      {
-        isbn: "9781449331818",
-        title: "Rohan Bhagwat",
-        subTitle: "QA Trainee",
-        author: "Rahul Gupta",
-        publish_date: "2020-06-12T08:48:39.000Z",
-        publisher: "Zeus Learning",
-        pages: 234,
-        description: "This is Dummy Data",
-        website: "https://playwright.dev/python/",
-      },
-    ],
-  };
 
   constructor(page: Page) {
     this.page = page;
   }
 
   // Mock successful response with custom data
-  async mockValidBooksResponse(customData = this.mockBookData) {
+  async mockValidBooksResponse(customData = mockBookData) {
     await this.page.route("**/BookStore/v1/Books", async (route) => {
       await route.fulfill({
         status: 200,
@@ -54,7 +38,7 @@ export class BookMockUtils {
       await route.fulfill({
         status: 200,
         contentType: "application/json",
-        body: JSON.stringify(this.mockBookData),
+        body: JSON.stringify(mockBookData),
       });
     });
   }
